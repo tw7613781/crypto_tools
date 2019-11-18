9fac6b445a47146ca5fd2b58451921d21a8217bf
Max Tang <tw7613781@gmail.com>
2019-11-18 10:56:41 +0900

    chore: using request in api server

--- src/bitcoke.ts
+++ src/bitcoke.ts
@@ -1,20 +1,23 @@
-import * as request from 'request-promise-native'
+import * as request from 'request'
 
 export class Bitcoke {
 
     // symbol can be BTCUSD, ETHUSD, EOSUSD
-    public async getPrice(symbol: string) {
-        const baseUrl = 'http://api.bitcoke.com/api/index/price'
-        const queryString = `?symbols=${symbol}`
-        const options = {
-            uri: baseUrl + queryString,
-        }
-        try {
-            let ret = await request.get(options)
-            ret = JSON.parse(ret)
-            return ret.result[0].value
-        } catch (err) {
-            throw err
-        }
+    public async getPrice(symbol: string): Promise<number> {
+        return new Promise( (resolve, reject) => {
+            const baseUrl = 'http://api.bitcoke.com/api/index/price'
+            const queryString = `?symbols=${symbol}`
+            const options = {
+                uri: baseUrl + queryString,
+            }
+            request.get(options, (err, res, body) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    const ret = JSON.parse(body)
+                    resolve(ret.result[0].value)
+                }
+            })
+        })
     }
 }
