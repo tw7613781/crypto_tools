bfac84a365d8df91fa241d372a2f2190fc99a40e
Max Tang <tw7613781@gmail.com>
2019-11-20 18:02:57 +0900

    feat: add new http header

--- src/baseCrawler.ts
+++ src/baseCrawler.ts
@@ -87,7 +87,12 @@ export abstract class BaseCrawler {
                 forever: true,
                 gzip: true,
                 headers: {
-                    'User-Agent': 'Mozilla/5.0',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                    'Accept-Encoding': 'gzip, deflate',
+                    'Accept-Language': 'en-US,en;q=0.5',
+                    'Connection': 'keep-alive',
+                    'Host': this.urlOrigin.hostname,
+                    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/39.0',
                 },
                 url: urlString,
             }
